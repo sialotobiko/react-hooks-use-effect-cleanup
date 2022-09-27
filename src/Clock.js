2f64bb2dc50b0af8396328fb0eb0bff2fb00eeb1
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-09-27 17:49:24 +0300

    Completed Assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
+    const timerID = setInterval(() => {
+      setTime(new Date());      
     }, 1000);
+
+    return function cleanup(){
+      clearInterval(timerID);
+    }
   }, []);
+
 
   return <div>{time.toString()}</div>;
 }
